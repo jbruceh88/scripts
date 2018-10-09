5c8af9a8a6ce675b64320d866580bc4ac767081e
Jason Bruce-Halliwell <jason.bruce-halliwell@rapidspike.com>
2018-10-09 19:09:33 +0100

    added a unit test

---
+++ gitHooks/unitTest/BasicTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class BasictTest extends TestCase
+
+    public function testName()
+    {
+      $name = 'Jason';
+      $this->assertEquals($name,'Jason');
+    }
+
+    public function testNumber()
+    {
+      $number = '111111';
+      $this->assertEquals($number, '111111');
+    }
+}
+
+
+?>
